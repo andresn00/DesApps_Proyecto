b53e24c2eb869ce603136a89e8ddc6f5d9024569
andresn00 <andreslozada00@hotmail.com>
2021-03-16 20:23:34 -0500

    Joystick y movimiento

---
+++ Labo/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Config
+    [SerializeField] float moveSpeed = 5f;
+
+    //Cache
+    [SerializeField] Joystick leftJoystick;
+    [SerializeField] Joystick rightJoystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = leftJoystick.Horizontal * Time.deltaTime * moveSpeed;
+        var deltaZ = leftJoystick.Vertical * Time.deltaTime * moveSpeed;
+
+        Vector3 movement = new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z + deltaZ);
+        transform.position = movement;
+    }
+}
