66f5e6e9e8299d9288ee805b3126a797294fb0d6
andresn00 <andreslozada00@hotmail.com>
2021-03-18 18:49:07 -0500

    Merge branch 'alternate' into main

---
+++ Labo/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Components
+    Rigidbody rb;
+    
+    //Config
+    [SerializeField] float moveSpeed = 5f;
+    float rotationAngle = -45f;
+
+    //Cache
+    [SerializeField] Joystick leftJoystick;
+    [SerializeField] Joystick rightJoystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = leftJoystick.Horizontal * Time.deltaTime * moveSpeed;
+        var deltaZ = leftJoystick.Vertical * Time.deltaTime * moveSpeed;
+
+        transform.position = new Vector3(transform.position.x + deltaX, transform.position.y, transform.position.z + deltaZ);
+
+        CalculatePlayerRotation(leftJoystick.Horizontal, leftJoystick.Vertical);
+    }
+
+    private void CalculatePlayerRotation(float x, float y)
+    {
+        if (x != 0 && y != 0)
+        {
+            rotationAngle = Mathf.Rad2Deg * Mathf.Atan2(y, x);
+        }
+        transform.eulerAngles = Vector3.down * rotationAngle;
+    }
+}
