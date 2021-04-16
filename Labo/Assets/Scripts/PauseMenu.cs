afbee10e8f189e1319b8cd21010669bcab49c25c
tomasdelgado16 <tomas.delgadoc@hotmail.com>
2021-04-15 21:22:23 -0500

    Menu de pausa
    Elaboración del menú de pausa.

---
+++ Labo/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+    public GameObject pauseMenuPanel;
+
+    public void Continue()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void Menu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuPrincipal");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
