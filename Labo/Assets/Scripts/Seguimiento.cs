f2d9a32f226ded92307b632071e00478a65eb6a6
DonMiike <79928294+DonMiike@users.noreply.github.com>
2021-05-12 10:36:38 -0500

    Enemigo

---
+++ Labo/Assets/Scripts/Seguimiento.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seguimiento : MonoBehaviour
+{
+    public float visionRadius;
+    public float speed;
+    GameObject player;
+    Vector3 initialPosition;
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = initialPosition;
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < visionRadius) target = player.transform.position;
+        float fixedSpeed = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+        
+    }
+}
